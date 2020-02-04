f87cd080ed4f3eebcb212f1d17c51bba8bf37153
SebastienDelrueW <58477200+SebastienDelrueW@users.noreply.github.com>
2020-02-04 11:42:45 +0100

    partie 2

--- src/app/hereos/hereos.component.ts
+++ src/app/hereos/hereos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hereos',
@@ -9,15 +10,17 @@ import { Hero} from '../hero';
 
 
 export class HereosComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Marguerite Bond'
-  };
-
+  selectedHero: Hero;
+  heroes = HEROES;
+ 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+// src/app/mock-heroes.ts
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Ginette Bond"},
+    { id: 2, name: "M" },
+    { id: 3, name: "Q" },
+    { id: 4, name: "Goldfinger" },
+    { id: 5, name: "Robin" },
+    { id: 6, name: "Donald" },
+    { id: 7, name: "Michel Plouf" },
+    { id: 8, name: "Asterix" },
+    { id: 9, name: "Jean-Claude Bond" },
+];
